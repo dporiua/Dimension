95a659ddd83ce14a9e000d0c7b40a476fff5255a
AlexIdris <146952700+AlexIdris@users.noreply.github.com>
2024-02-24 08:40:17 +0700

    Level Expansion

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -6,15 +7,73 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private CameraToggle cameraToggle;
 
+    [SerializeField] private int maxhealth = 90;
+    [SerializeField] private int thirdhealth = 60;
+    [SerializeField] private int secondhealth = 30;
+    [SerializeField] private int firsthealth = 0;
+
+    [SerializeField] private float x = 0f;
+    [SerializeField] private float y = -1.8f;
+    [SerializeField] private float z = 29f;
+
+    [SerializeField] private int currenthealth = 0;
+    [SerializeField] private string damage = "Damage";
+    [SerializeField] private GameObject deathScene;
+    [SerializeField] Animator animator1;
+    [SerializeField] private Animator animator2;
+    [SerializeField] private Animator animator3;
+    [SerializeField] private Animator animator4;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         cameraToggle = FindObjectOfType<CameraToggle>(); // looks for the camera script
+        currenthealth = maxhealth;
     }
 
     void Update()
     {
         MovePlayer();
+
+    }
+
+    private async void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag(damage) && currenthealth == maxhealth)
+        {
+            animator4.SetBool("health4", true);
+            currenthealth = thirdhealth;
+            await Task.Delay(100);
+            transform.position = new Vector3(x, y, z);
+
+
+
+        }
+        if (collision.gameObject.CompareTag(damage) && currenthealth == thirdhealth)
+        {
+            animator3.SetBool("health3", true);
+            currenthealth = secondhealth;
+            await Task.Delay(100);
+            transform.position = new Vector3(x, y, z);
+
+
+        }
+        if (collision.gameObject.CompareTag(damage) && currenthealth == secondhealth)
+        {
+            animator2.SetBool("health2", true);
+            currenthealth = firsthealth;
+            await Task.Delay(100);
+            transform.position = new Vector3(x, y, z);
+
+
+        }
+        if (collision.gameObject.CompareTag(damage) && currenthealth == firsthealth)
+        {
+            animator1.SetBool("health1", true);
+            await Task.Delay(100);
+            deathScene.SetActive(true);
+
+        }
     }
 
     void MovePlayer()
@@ -22,9 +81,9 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        if (cameraToggle.IsOrthographic()) 
+        if (cameraToggle.IsOrthographic())
         {
-            Vector3 movement = new Vector3(0.0f, 0.0f, moveVertical); //restricts 2d movement in just one direction, change if misalligned
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f); //restricts 2d movement in just one direction, change if misalligned
             rb.velocity = movement * speed;
         }
         else
